fec54198e72896589ec3e731cc3d42fa74feacd6
Santosh Mirajkar <santosh.mirajkar@timesgroup.com>
2025-07-21 15:33:59 +0530

    requirement-packages

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup 
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="Santosh",
+    author_email="samirajkar99@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
