3658224f64515596e0bffbe8e8675be2951380e5
Santosh Mirajkar <santosh.mirajkar@timesgroup.com>
2025-07-21 11:34:31 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,14 @@
+#create directories for the project structure
+mkdir -p src
+mkdir -p reasearch
+# creating files 
+touch src/_init_.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch reasearch/trials.ipynb
+touch requirements.txt
+
+echo "Project structure created successfully!."
